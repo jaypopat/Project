6555f7aa38245d903089af1756403f65ff3422f0
Jeffrey Reynolds <j.reynolds14@universityofgalway.ie>
2023-10-12 14:37:08 +0100

    Adds basic auth methods

---
+++ backend/auth/login.js
@@ -0,0 +1,14 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    return user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+  });
