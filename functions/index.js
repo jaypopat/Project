6f531b33c7e5e736167a582e00d8dda7cfd7984c
Jeffrey Reynolds <j.reynolds14@universityofgalway.ie>
2023-10-26 14:44:26 +0100

    Adds functionality for cloud functions

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+//const {onCall} = require("firebase-functions/v2/https");
+//const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.helloWorld = onRequest((request, response) => {
+   logger.info("Hello logs!!!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
