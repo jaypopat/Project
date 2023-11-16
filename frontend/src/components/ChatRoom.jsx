537cb8f436ffda59608797ef02d9148311534661
jaypopat <contact.jaypopat@gmail.com>
2023-11-16 13:09:47 +0000

    added chatroom ui

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -1,29 +1,92 @@
-import "./ChatRoom.css";
-import { useParams } from "react-router-dom";
+import {
+  MainContainer,
+  Sidebar,
+  ConversationList,
+  ChatContainer,
+  ConversationHeader,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import Rooms from "./Rooms";
+import { Link } from "react-router-dom";
+import './ChatRoom.css';
+
+const messages = [
+  {
+    id: "1",
+    text: "Hello!",
+    createdAt: new Date(),
+    uid: "user1",
+    avatar: "https://i.pravatar.cc/50?img=1",
+  },
+  {
+    id: "2",
+    text: "Hi!",
+    createdAt: new Date(),
+    uid: "user2",
+    avatar: "https://i.pravatar.cc/50?img=2",
+  },
+  {
+    id: "3",
+    text: "How are you?",
+    createdAt: new Date(),
+    uid: "user1",
+    avatar: "https://i.pravatar.cc/50?img=3",
+  },
+];
 
 function ChatRoom() {
-  const { id } = useParams();
-  console.log(id); // look up the room name correseponding to the number
 
   return (
-    <>
-      <div style={{ display: "flex" }}>
-        <Rooms />
-        <div className="chat-container">
-          <div className="chat-header">
-            <h2>Room Name</h2>
-          </div>
-          <div className="chat-messages"></div>
-          <div className="chat-input">
-            <input type="text" placeholder="Type a message..." />
-            <button>Send</button>
-          </div>
-        </div>
+    <div className="chatroom">
+      <div className="chatroom-header">
+        <h2>Room name (will be fetched from uuid)</h2>
+        <Link
+          to="/joinroom"
+          className="chatroom-header-back"
+        >
+          ⬅️ Back to all rooms
+        </Link>
       </div>
-    </>
+      <div className="chatroom-body">
+        <MainContainer>
+          <Sidebar position="left">
+            <ConversationList>
+              <Rooms/>
+            </ConversationList>
+          </Sidebar>
+          <ChatContainer>
+            <ConversationHeader>
+              Room Name
+            </ConversationHeader>
+            <MessageList>
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  className="message"
+                >
+                  <div
+                    className="avatar"
+                    style={{ backgroundImage: `url(${message.avatar})` }}
+                  />
+                  <Message
+                    model={{
+                      message: message.text,
+                      sentTime: message.createdAt,
+                      sender: message.uid,
+                    }}
+                  />
+                </div>
+              ))}
+            </MessageList>
+            <MessageInput placeholder="Type message here" />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
   );
 }
 
 export default ChatRoom;
-
