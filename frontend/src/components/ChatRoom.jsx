236559c1220e260209ae2ae71aaf0df1f2a5561e
Jay Popat <contact.jaypopat@gmail.com>
2024-03-13 21:36:15 +0000

    added maps feature

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -5,46 +5,65 @@ import {
   ChatContainer,
   ConversationHeader,
   MessageList,
-  Message,
   MessageInput,
 } from "@chatscope/chat-ui-kit-react";
 import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import Rooms from "./Rooms";
-import {Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./ChatRoom.css";
-import {useEffect, useState} from "react";
-import {addDoc, collection, doc, onSnapshot, orderBy, query, serverTimestamp} from "firebase/firestore";
-import {auth, db} from "../../firebase";
+import { useEffect, useState } from "react";
+import { addDoc, collection, getDoc, doc, onSnapshot, orderBy, query } from "firebase/firestore";
+import { auth, db } from "../../firebase";
 
 function ChatRoom() {
   const { id: roomId } = useParams();
   const [newMessage, setNewMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [roomName, setRoomName] = useState("");
 
 
+  const getRoomName = async (roomId) => {
+    try {
+      const docRef = doc(db, "rooms", roomId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Room Name:", docSnap.data().name);
+        return docSnap.data().name;
+      } else {
+        console.log("No such document!");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching document:", error);
+      return null;
+    }
+  }
   useEffect(() => {
     if (!roomId) return;
-   
+
     const roomRef = doc(db, "rooms", roomId);
     const messagesRef = collection(roomRef, "messages");
-   
+
     const q = query(messagesRef, orderBy("createdAt", "asc"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
-       snapshot.docChanges().forEach((change) => {
-         if (change.type === "added") {
-           setMessages((prevMessages) => [
-             ...prevMessages,
-             {
-               ...change.doc.data(),
-               id: change.doc.id,
-             },
-           ]);
-         }
-       });
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            {
+              ...change.doc.data(),
+              id: change.doc.id,
+            },
+          ]);
+        }
+      });
     });
-   
+
+    getRoomName(roomId).then(name => setRoomName(name));
+
     return () => unsubscribe();
-   }, [roomId]);
+  }, [roomId]);
 
   const handleSubmit = async (messageText) => {
     if (!messageText.trim()) return;
@@ -54,7 +73,7 @@ function ChatRoom() {
 
       await addDoc(messagesRef, {
         text: messageText,
-        createdAt: serverTimestamp(),
+        createdAt: new Date,
         user: auth.currentUser.displayName,
         userPic: auth.currentUser.photoURL,
       });
@@ -64,11 +83,21 @@ function ChatRoom() {
     }
   }
 
+  const formatDate = (date) => {
 
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${hours}:${minutes} ${ampm}`;
+  }
   return (
     <div className="chatroom">
       <div className="chatroom-header">
-        <h2>Room name (will be fetched from uuid)</h2>
+        <h2>{roomName}</h2>
         <Link to="/joinroom" className="chatroom-header-back">
           ⬅️ Back to all rooms
         </Link>
@@ -83,47 +112,26 @@ function ChatRoom() {
           <ChatContainer>
             <ConversationHeader>Room Name</ConversationHeader>
             <MessageList>
-
-              {/* replace with nicer display*/}
               <div className="messages">
                 {messages.map((message) => (
-                    <div className="message" key={message.id}>
-                      <span className="user">{message.user} : </span>
-                      { message.text}
+                  <div className="message-container" key={message.id}>
+                    <img src={message.userPic} alt="pfp" className="user-pic" />
+                    <div className="message-content">
+                      <span className="user-name">{message.user}</span>
+                      <p className="message-text">{message.text}</p>
+                      <p className="message-timestamp">
+                        {/* {console.log(message.createdAt)} */}
+                      </p>
                     </div>
+                  </div>
                 ))}
-                </div>
-
-
-              {/*{messages.map((message, index) => (*/}
-              {/*  <div key={index} className="message">*/}
-              {/*    <p>{`${message.createdAt*/}
-              {/*      .getHours()*/}
-              {/*      .toString()*/}
-              {/*      .padStart(2, "0")}:${message.createdAt*/}
-              {/*      .getMinutes()*/}
-              {/*      .toString()*/}
-              {/*      .padStart(2, "0")}`}</p>*/}
-
-              {/*    <div*/}
-              {/*      className="avatar"*/}
-              {/*      style={{ backgroundImage: `url(${message.avatar})` }}*/}
-              {/*    />*/}
-              {/*    <Message*/}
-              {/*      model={{*/}
-              {/*        message: message.text,*/}
-              {/*        sentTime: message.createdAt,*/}
-              {/*        sender: message.uid,*/}
-              {/*      }}*/}
-              {/*    />*/}
-              {/*  </div>*/}
-              {/*))}*/}
+              </div>
             </MessageList>
             <MessageInput
-                value={newMessage}
-                onChange={(val) => setNewMessage(val)}
-                onSend={handleSubmit}
-                placeholder="Type message here"
+              value={newMessage}
+              onChange={(val) => setNewMessage(val)}
+              onSend={handleSubmit}
+              placeholder="Type message here"
             />
           </ChatContainer>
         </MainContainer>
@@ -131,5 +139,4 @@ function ChatRoom() {
     </div>
   );
 }
-
 export default ChatRoom;
--- frontend/src/components/Rooms.jsx
+++ frontend/src/components/Rooms.jsx
@@ -1,70 +1,122 @@
-/* eslint-disable react/prop-types */
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import "./Rooms.css";
 import "react-toastify/dist/ReactToastify.css";
 import Spinner from "./Spinner";
 import { Link } from "react-router-dom";
 import { UserContext } from "../App";
-import { calculateDistance } from "../utils/calculateDistance"; // Ensure this matches your actual utility function name and path
+import { calculateDistance } from "../utils/calculateDistance";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../../firebase";
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+mapboxgl.accessToken = "pk.eyJ1IjoiamF5cDEiLCJhIjoiY2x0cTh4MWR0MDNsbTJpcGNpYWpvcWlsNSJ9.ebl8ozkVTD0hNk8mAKzDcQ";
 
 const Rooms = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [showMapBox, setShowMap] = useState(false); 
   const { userLocation } = useContext(UserContext);
   const [rooms, setRooms] = useState([]);
-  const roomsRef = collection(db, "rooms");
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
 
   useEffect(() => {
+    const roomsRef = collection(db, "rooms");
     const queryRooms = query(roomsRef, orderBy("createdAt", "asc"));
+    setIsLoading(true)
     const unsubscribe = onSnapshot(queryRooms, (snapshot) => {
       let filteredRooms = [];
       snapshot.forEach((doc) => {
         const roomData = { ...doc.data(), id: doc.id };
-        //console.log(roomData);
-        if (userLocation && roomData.createdLocation) { // Ensure there's a check for roomData.createdLocation existence
+        if (userLocation && roomData.createdLocation) {
           const distance = calculateDistance(
-              userLocation.latitude,
-              userLocation.longitude,
-              roomData.createdLocation.latitude,
-              roomData.createdLocation.longitude
+            userLocation.latitude,
+            userLocation.longitude,
+            roomData.createdLocation.latitude,
+            roomData.createdLocation.longitude
           );
           if (distance <= roomData.radius) {
             filteredRooms.push(roomData);
           }
         }
       });
+      setIsLoading(false);
       setRooms(filteredRooms);
     });
     return () => unsubscribe();
   }, [userLocation]);
 
-  return isLoading ? (
-      <Spinner />
-  ) : error ? (
-      <div id="error">{error}</div>
-  ) : (
+  const showMap = (centrePoint, radius) => {
+    setShowMap(true);
+    if (!map.current) {
+       map.current = new mapboxgl.Map({
+         container: mapContainer.current,
+         style: 'mapbox://styles/mapbox/streets-v12',
+         center: centrePoint,
+         zoom: 12,
+       });
+   
+       map.current.on('load', () => {
+         // Add a GeoJSON source for the circle
+         map.current.addSource("circleSource", {
+           "type": "geojson",
+           "data": {
+             "type": "FeatureCollection",
+             "features": [{
+               "type": "Feature",
+               "geometry": {
+                 "type": "Point",
+                 "coordinates": centrePoint
+               }
+             }]
+           }
+         });
+   
+         map.current.addLayer({
+           "id": "circleLayer",
+           "type": "circle",
+           "source": "circleSource",
+           "paint": {
+             "circle-radius": radius*1000, // Specify the radius in meters
+             "circle-color": "#ff0000", // Red color
+             "circle-opacity": 0.6
+           }
+         });
+       });
+    } else {
+       map.current.setCenter(centrePoint);
+       map.current.setZoom(12);
+    }
+   }
+   
+
+  return isLoading ? <Spinner /> : (
+    <div>
       <table id="rooms">
         <thead>
-        <tr>
-          <th>Room Name</th>
-          <th>Action</th>
-        </tr>
+          <tr>
+            <th>Room Name</th>
+            <th>Action</th>
+          </tr>
         </thead>
         <tbody>
-        {rooms.map((room) => (
+          {rooms.map((room) => (
             <tr key={room.id}>
               <td>{room.name}</td>
               <td>
                 <Link to={`/joinroom/${room.id}`}>
                   <button className="join-button">Join</button>
                 </Link>
+                <button onClick={() => showMap([room.createdLocation.latitude, room.createdLocation.longitude], room.radius)} className="join-button">View on Map</button>
               </td>
             </tr>
-        ))}
+          ))}
+          <tr>
+          </tr>
         </tbody>
       </table>
+      {showMap && <div ref={mapContainer} id="map" />}
+    </div>
   );
 };
 
