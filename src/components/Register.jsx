f701fb5ec7958ded277aeeff1fff78bd9aea9917
joeyg <joeygalvin2134@gmail.com>
2024-03-21 17:39:29 +0000

    email verification added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,6 @@
-import {useEffect, useState } from "react";
-// import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  auth,
-  // auth,
-  registerWithEmailAndPassword,
-  signInWithGoogle,
-} from "../firebaseAuth.js";
+import { auth, registerWithEmailAndPassword, signInWithGoogle } from "../firebaseAuth.js";
 import "./Register.css";
 import { useNavigate } from "react-router-dom";
 
@@ -16,59 +10,68 @@ function Register() {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
-  const register = () => {
-    if (!name) alert("Please enter name");
-    registerWithEmailAndPassword(name, email, password);
+  const register = async () => {
+    if (!name) {
+      alert("Please enter name");
+      return;
+    }
+    try {
+      await registerWithEmailAndPassword(name, email, password);
+    } catch (error) {
+    }
   };
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        navigate("/");
+        if (user.emailVerified) {
+          navigate("/login");
+        }
       }
     });
 
-    // Cleanup subscription on unmount
     return () => unsubscribe();
   }, []);
 
   return (
-    <div className="register">
-      <div className="register__container">
-        <input
-          type="text"
-          className="register__textBox"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Full Name"
-        />
-        <input
-          type="text"
-          className="register__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="register__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button className="register__btn" onClick={register}>
-          Register
-        </button>
-        <button
-          className="register__btn register__google"
-          onClick={signInWithGoogle}        >
-          Register with Google
-        </button>
-        <div>
-          Already have an account? <Link to="/login">Login</Link> now.
+      <div className="register">
+        <div className="registercontainer">
+          <input
+              type="text"
+              className="registertextBox"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Full Name"
+          />
+          <input
+              type="text"
+              className="registertextBox"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="E-mail Address"
+          />
+          <input
+              type="password"
+              className="registertextBox"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+          />
+          <button className="registerbtn" onClick={register}>
+            Register
+          </button>
+          <button
+              className="registerbtn register__google"
+              onClick={signInWithGoogle}
+          >
+            Register with Google
+          </button>
+          <div>
+            Already have an account? <Link to="/login">Login</Link> now.
+          </div>
         </div>
       </div>
-    </div>
   );
 }
+
 export default Register;
--- src/firebaseAuth.js
+++ src/firebaseAuth.js
@@ -1,23 +1,23 @@
 import { initializeApp } from "firebase/app";
 import {
-  GoogleAuthProvider,
-  getAuth,
-  signInWithPopup,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
+    GoogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+    signOut,
 } from "firebase/auth";
 import { updateProfile } from "firebase/auth";
 
 
 import {
-  getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
 } from "firebase/firestore";
 import { toast } from "react-toastify";
 
@@ -37,78 +37,78 @@ export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 
 export const signInWithGoogle = async () => {
-  try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    const docs = await getDocs(q);
-    if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
-        uid: user.uid,
-        name: user.displayName,
-        authProvider: "google",
-        email: user.email,
-      });
-    }    
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+        if (docs.docs.length === 0) {
+            await addDoc(collection(db, "users"), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
 
-  }
+    }
 };
 
 export const logInWithEmailAndPassword = (email, password) => {
- signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      return user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode, errorMessage);
-    });
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            return user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        });
 };
 
 export const registerWithEmailAndPassword = async (name, email, password) => {
-  try {
-     const res = await createUserWithEmailAndPassword(auth, email, password);
-     const user = res.user;
-     console.log(user);
-     await addDoc(collection(db, "users"), {
-       uid: user.uid,
-       name,
-       authProvider: "email",
-       email,
-     });
- 
-     // Update the user's display name and profile picture
-     await updateProfile(user, {
-       displayName: name,
-       photoURL: `https://api.dicebear.com/8.x/pixel-art/svg?seed=${name}`
-     });
- 
-     // Reload the user object to ensure it's up-to-date
-     await user.reload();
- 
-     console.log("User registered and profile updated successfully");
-  } catch (err) {
-     console.error(err);
-     alert(err.message);
-  }
- };
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        console.log(user);
+        await addDoc(collection(db, "users"), {
+            uid: user.uid,
+            name,
+            authProvider: "email",
+            email,
+        });
+
+        // Update the user's display name and profile picture
+        await updateProfile(user, {
+            displayName: name,
+            photoURL: `https://api.dicebear.com/8.x/pixel-art/svg?seed=${name}`
+        });
+
+        // Reload the user object to ensure it's up-to-date
+        await user.reload();
+
+        console.log("User registered and profile updated successfully");
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
 
 export const sendPasswordReset = async (email) => {
-  try {
-    await sendPasswordResetEmail(auth, email);
-    alert("Password reset link sent!");
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
- };
- 
+    try {
+        await sendPasswordResetEmail(auth, email);
+        alert("Password reset link sent!");
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
 export const logout = () => {
-  signOut(auth);
-  toast.success("signing out")
+    signOut(auth);
+    toast.success("signing out")
 };
