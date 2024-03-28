caf2d1381fc293dd07ff25d787f530f0dd80f8c3
Jake <jakeogrady15@gmail.com>
2024-03-28 09:16:06 +0000

    Some basic changes of buttons and mobile responsiveness across a few pages such as userProfile and friendList

--- src/components/FriendRequests.jsx
+++ src/components/FriendRequests.jsx
@@ -1,13 +1,13 @@
-import {useContext, useEffect, useState} from "react";
-import {UserContext} from "../App.jsx";
-import {collection, doc, getDocs, query, addDoc, deleteDoc } from "firebase/firestore";
-import {db} from "../firebaseAuth.js";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../App.jsx";
+import { collection, doc, getDocs, query, addDoc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebaseAuth.js";
 import { Link } from 'react-router-dom';
 import "./FriendRequests.css"
 
 const FriendRequests = () => {
     const { user } = useContext(UserContext);
-    const [ friendRequests, setFriendRequests ] = useState([]);
+    const [friendRequests, setFriendRequests] = useState([]);
 
     useEffect(() => {
         const getFriendRequests = async () => {
@@ -68,21 +68,25 @@ const FriendRequests = () => {
     }
 
     if (friendRequests.length === 0) return (
-    <div className="no-requests">
-        <h1 className="no-requests-msg">Womp Womp</h1>
-        <p className="empty-state">You have no current friend requests pending</p>
-        <Link to="/joinroom" className='join-link'><button className='find-room'>Join a Room</button></Link>
-    </div>
+        <div className="no-requests">
+            <h1 className="no-requests-msg">Womp Womp</h1>
+            <p className="empty-state">You have no current friend requests pending</p>
+            <Link to="/joinroom" className='join-link'><button className='find-room'>Join a Room</button></Link>
+        </div>
     );
     return (
-        <div>
-            <h1>Friend Requests</h1>
+        <div className="container-request-friends">
+            <h1 className="header-requests">Friend Requests</h1>
             <ul>
                 {friendRequests.map((request) => (
                     <li key={request.id}>
                         <p>{request.friendName}</p>
-                        <button onClick={() => handleAccept(request.id)}>Accept</button>
-                        <button onClick={() => handleDecline(request.id)}>Decline</button>
+                        <div className="button-holder">
+                            <button className="accept-friend"
+                                onClick={() => handleAccept(request.id)}>Accept</button>
+                            <button className="decline-friend"
+                                onClick={() => handleDecline(request.id)}>Decline</button>
+                        </div>
                     </li>
                 ))}
             </ul>
