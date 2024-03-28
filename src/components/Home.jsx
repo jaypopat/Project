2e4417fcf8ed20b5e0b43fe439e5d3f311fd59ce
Jake <jakeogrady15@gmail.com>
2024-03-28 09:42:43 +0000

    Fixed Register Page Responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,7 +29,13 @@ const Home = () => {
         </nav>
         <div className="content">
             <h1 className="slogan">Keep it Close.</h1>
-            <p className="info">Welcome to Warp, the innovative chat app that brings you closer to the people around you. Our platform is designed to connect you with individuals in your immediate vicinity, fostering community and enabling real-time interactions. Whether you're at a cafe, attending a conference, or exploring a new neighborhood, Warp empowers you to discover and engage with nearby users. Experience seamless communication and make meaningful connections, right where you are.</p>
+            <p className="info">Welcome to Warp, the innovative chat app 
+            that brings you closer to the people around you.
+             Whether you're at a cafe, attending 
+             a conference, or exploring a new neighborhood, Warp empowers
+              you toengage with nearby users. Experience 
+              seamless communication and make meaningful connections, right
+               where you are.</p>
             <Link to="/about"><button className="btn-learn-more phase-in">Learn More</button></Link>
         </div>
     </div>
